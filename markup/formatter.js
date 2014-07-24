29486bf45137cf68e781495439ad5f6ae8e35501
_Tobias <lupuswolfram@gmail.com>
2014-07-24 19:39:40 +0200

    Some updates to the formatter

--- markup/formatter.js
+++ markup/formatter.js
@@ -1,8 +1,44 @@
 String.prototype.format = function() {
 	var text = "";
 	var pause = false;
+	var snippet = -1;
+	var snippets = {};
+	var replacements = {};
 	this.split("\n").forEach(function(v) {
 		var v = v.trim();
+
+		var snippet_cmd = v.match(/^snippet ([0-9]+) ([begin|end|insert]+)(.*?)$/);
+		if(snippet_cmd) {
+			if(snippet_cmd[2] == 'begin')
+				snippet = snippet_cmd[1];
+			else if(snippet_cmd[2] == 'end')
+				snippet = -1;
+			else if(snippet_cmd[2] == 'insert') {
+				var str = snippets[snippet_cmd[1]].join("\n");
+				if(snippet_cmd[3]) {
+					var args = snippet_cmd[3].trim().split('; ');
+					args.forEach(function(v,k) {
+						str = str.split('{{'+k+'}}').join(v);
+					});
+				}
+				text += str.format();
+			}
+			return;
+		}
+
+		if(snippet != -1) {
+			if(!snippets[snippet])
+				snippets[snippet] = [];
+			snippets[snippet].push(v.replace('<!--', '').replace('-->', ''));
+			return;
+		}
+
+		var replace = v.match(/^replace (.+?) with (.+)$/);
+		if(replace) {
+			replacements[replace[1]] = replace[2];
+			return;
+		}
+
 		if(v == 'pause formatting') {
 			pause = true;
 			return;
@@ -10,6 +46,12 @@ String.prototype.format = function() {
 		else if(v == 'resume formatting') {
 			pause = false;
 			return;
+		}
+
+		for (var replace in replacements) {
+			if(replacements.hasOwnProperty(replace)) {
+				v = v.split(replace).join(replacements[replace]);
+			}
 		}
 
 		v = v.autoLink({

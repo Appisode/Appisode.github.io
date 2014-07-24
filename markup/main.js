acfd52a076762c6aa1b5644d6dc859c006c63fec
_Tobias <lupuswolfram@gmail.com>
2014-07-24 19:43:38 +0200

    Formatter source invisible on load

--- markup/main.js
+++ markup/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
 	var textElement = document.getElementById('text');
 	textElement.innerHTML = textElement.innerHTML.format();
+	textElement.style.display = 'block';
 };

dd12f61e4f0a96e60ebdb2021369db2bdbb6dbc3
Tobias <lupuswolfram@gmail.com>
2014-11-06 23:39:51 +0100

    Github commit view implemented

--- markup/main.js
+++ markup/main.js
@@ -9,4 +9,36 @@ window.onload = function() {
 			location.hash = hash;
 		}
 	})(location.hash);
+
+	var commitsUnfold = document.getElementById('commits-unfold');
+	var commitsArrow = document.getElementById('commits-arrow');
+	var commitsHidden = document.getElementById('commits-hidden');
+	commitsUnfold.onclick = function() {
+		if(commitsArrow.className === 'arrow-down') {
+			commitsArrow.className = 'arrow-up';
+			commitsHidden.className = '';
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+		else if(commitsArrow.className === 'arrow-up') {
+			commitsArrow.className = 'arrow-down';
+			commitsHidden.className = 'hidden';
+		}
+	};
+};
+
+window._gh_commits = function(data) {
+	var count = 0;
+	var commits = document.getElementById('commits-hidden');
+	if(data.meta['status'] !== 200)
+		return;
+	document.getElementById('commits').style.display = 'block';
+	data.data.every(function(v) {
+		console.log(v.sha.substr(0, 7));
+		var div = document.createElement('DIV');
+		div.className = 'commit';
+		div.innerHTML = '<a href="'+v.html_url+'" target="_blank">'+v.sha.substr(0, 7)+'</a>: '+v.commit.message;
+		commits.appendChild(div);
+		count++;
+		return count < 5;
+	});
 };
